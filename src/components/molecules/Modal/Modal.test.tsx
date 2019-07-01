327668a86b5f170be2d17a8b3710ddcb8a1a689e
Lluis Agusti <lluis.agusti@zopa.com>
2019-07-01 13:10:41 +0200

    test(modal): improve coverage

--- src/components/molecules/Modal/Modal.test.tsx
+++ src/components/molecules/Modal/Modal.test.tsx
@@ -3,10 +3,39 @@ import { render, waitForElement } from 'react-testing-library';
 import Modal from './Modal';
 
 describe('<Modal />', () => {
-  it('render the opened Modal', async () => {
+  it('can render un-opened', async () => {
+    Modal.setAppElement('body');
+    const { queryByText } = render(<Modal isOpen={false}>text</Modal>);
+
+    expect(queryByText('text')).toBe(null);
+  });
+
+  it('can render opened', async () => {
     Modal.setAppElement('body');
     const { getByText } = render(<Modal isOpen>text</Modal>);
+
     const modal = await waitForElement(() => getByText('text'));
     expect(modal).toMatchSnapshot();
   });
+
+  it('allows to set global styles', () => {
+    render(<Modal.Styles />);
+
+    const [globalModalStyles] = getGlobalStyleTags();
+    expect(globalModalStyles).toMatchSnapshot();
+  });
+
+  it('allows to set the `z-index` for the modal portal', () => {
+    const expectedZIndex = 100;
+    render(<Modal.Styles zIndex={expectedZIndex} />);
+
+    const [globalModalStyles] = getGlobalStyleTags();
+    expect(globalModalStyles).toContain(`z-index:${expectedZIndex}`);
+  });
 });
+
+function getGlobalStyleTags() {
+  return Array.from(document.querySelectorAll('style')).map(styleTag =>
+    styleTag.innerHTML.trim().replace(/\s+/gm, ' '),
+  );
+}
