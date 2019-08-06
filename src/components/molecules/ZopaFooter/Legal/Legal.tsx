a151a9de4b89592d075b800bc0d8850e7ee0e623
rominamoya <rominamoya@gmail.com>
2019-08-06 10:09:04 +0100

    fix(footer): mock date on tests
    mock date on test to prevent failure  test every new year

--- src/components/molecules/ZopaFooter/Legal/Legal.tsx
+++ src/components/molecules/ZopaFooter/Legal/Legal.tsx
@@ -12,26 +12,28 @@ const StyledParagraph = styled.p`
   margin: 0;
 `;
 
-const content = {
-  about: `Zopa Limited is incorporated in England & Wales (registration number 05197592), with its registered office at 1st Floor, Cottons Centre, Tooley Street, 
-London, SE1 2QG. Zopa Limited is authorised and regulated by the Financial Conduct Authority, 
-and entered on the Financial Services Register under firm registration number 718925.`,
+const Legal = () => {
+  const { about, rightsReserved } = {
+    about: `Zopa Limited is incorporated in England & Wales (registration number 05197592), with its registered office at 1st Floor, Cottons Centre, Tooley Street, 
+    London, SE1 2QG. Zopa Limited is authorised and regulated by the Financial Conduct Authority, 
+    and entered on the Financial Services Register under firm registration number 718925.`,
 
-  rightsReserved: `© Zopa Limited ${new Date().getFullYear()} All rights reserved. 'Zopa' and the Zopa logo are trade marks of Zopa Limited. 
-Zopa is a member of CIFAS – the UK's leading anti-fraud association, 
-and we are registered with the Office of the Information Commissioner (No. Z8797078).`,
+    rightsReserved: `© Zopa Limited ${new Date().getFullYear()} All rights reserved. 'Zopa' and the Zopa logo are trade marks of Zopa Limited. 
+    Zopa is a member of CIFAS – the UK's leading anti-fraud association, 
+    and we are registered with the Office of the Information Commissioner (No. Z8797078).`,
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <StyledParagraph>{about}</StyledParagraph>
+      </Wrapper>
+      <Separator />
+      <Wrapper>
+        <StyledParagraph>{rightsReserved}</StyledParagraph>
+      </Wrapper>
+    </>
+  );
 };
-
-const Legal = () => (
-  <>
-    <Wrapper>
-      <StyledParagraph>{content.about}</StyledParagraph>
-    </Wrapper>
-    <Separator />
-    <Wrapper>
-      <StyledParagraph>{content.rightsReserved}</StyledParagraph>
-    </Wrapper>
-  </>
-);
 
 export default Legal;
