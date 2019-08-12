8d05f29469225a58d78a91f99fe45e8a375b9bb8
Lluis Agusti <lluis.agusti@zopa.com>
2019-08-12 14:07:55 +0200

    feat(fonts): add new typographic rules
    New typographic rules following the re-branding update at Zopa. We now have constraints around text
    size, weight and line-height. Alverate, the font previously used on headings is now removed in
    favour of Open Sans.
    
    BREAKING CHANGE: You won't be able to see Alverate font anymore in the components. Default
    typography applied to the page through the CSS cascade has changed as well to reflect the new
    typographic rules.
    
    fix #51

---
+++ src/constants/typography.ts
@@ -0,0 +1,27 @@
+const systemFontStack =
+  '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+
+const typography = {
+  primary: `"Open Sans",${systemFontStack}`,
+  sizes: {
+    text: {
+      1: '16px',
+      2: '14px',
+      3: '12px',
+    },
+    heading: {
+      h1: '48px',
+      h2: '28px',
+      h3: '24px',
+      h4: '20px',
+    },
+  },
+  weights: {
+    regular: 400,
+    semiBold: 600,
+    bold: 700,
+  },
+  lineHeight: 1.6,
+};
+
+export { typography };
