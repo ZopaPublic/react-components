fa92a3368672923c50a3a022a0ede7f217dd0cfb
thegrinder <paul.szczepkowski@gmail.com>
2019-08-26 11:30:01 +0200

    test(form.textfield): test it

---
+++ src/components/organisms/Form/FormTextField/FormTextField.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { act, fireEvent, render } from '@testing-library/react';
+import Form from '../Form';
+
+interface TForm {
+  firstName: string;
+}
+
+const onSubmit = jest.fn();
+const buttonLabel = 'continue';
+const fieldLabel = 'First name';
+const errorMessage = 'This field is required';
+
+const validate = (values: TForm) => {
+  let errors: Partial<TForm> = {};
+  if (!values.firstName) {
+    errors.firstName = errorMessage;
+  }
+  return errors;
+};
+
+const renderComponent = () =>
+  render(
+    <Form.Provider initialValues={{ firstName: '' }} validate={validate} onSubmit={onSubmit}>
+      <Form.TextField label={fieldLabel} name="firstName" />
+      <Form.Button>{buttonLabel}</Form.Button>
+    </Form.Provider>,
+  );
+
+describe('<Form.TextField />', () => {
+  it('handles value change', () => {
+    const { getByText, getByLabelText } = renderComponent();
+    const value = 'name';
+    act(() => {
+      fireEvent.change(getByLabelText(fieldLabel), { target: { value } });
+    });
+    act(() => {
+      fireEvent.click(getByText(buttonLabel));
+    });
+    expect(onSubmit).toHaveBeenCalledWith({ firstName: value });
+  });
+
+  it('renders error message', () => {
+    const { queryByText, getByLabelText } = renderComponent();
+    const textField = getByLabelText(fieldLabel);
+    act(() => {
+      fireEvent.click(textField);
+    });
+    act(() => {
+      fireEvent.blur(textField);
+    });
+    expect(queryByText(errorMessage)).toBeTruthy();
+  });
+});
