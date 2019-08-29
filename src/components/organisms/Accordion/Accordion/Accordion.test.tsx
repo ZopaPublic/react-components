cc2ac279eb1e495e09342ba2a4aaca5ef8601ec9
thegrinder <paul.szczepkowski@gmail.com>
2019-08-29 17:08:03 +0200

    test(accordion): fix query

--- src/components/organisms/Accordion/Accordion/Accordion.test.tsx
+++ src/components/organisms/Accordion/Accordion/Accordion.test.tsx
@@ -5,7 +5,7 @@ import Accordion from '..';
 describe('<Accordion />', () => {
   it('should render aria-label', () => {
     const ariaLabel = 'test-accordion';
-    const { queryByText } = render(<Accordion aria-label={ariaLabel} />);
-    expect(queryByText).toBeTruthy();
+    const { queryByLabelText } = render(<Accordion aria-label={ariaLabel} />);
+    expect(queryByLabelText(ariaLabel)).toBeTruthy();
   });
 });
