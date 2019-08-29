e80cfba44f6ca9d218a64bfaa78ac29fb5948fef
thegrinder <paul.szczepkowski@gmail.com>
2019-08-29 12:23:40 +0200

    test(useaccordioncontext): test it

---
+++ src/components/organisms/Accordion/context/useAccordionContext.test.tsx
@@ -0,0 +1,12 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useAccordionContext from './useAccordionContext';
+
+describe('useAccordionContext', () => {
+  it('should throw if no context present', () => {
+    const { result } = renderHook(() => {
+      useAccordionContext();
+    });
+    expect(() => result.current).toThrow('useAccordionContext must be used within an Accordion');
+  });
+});
