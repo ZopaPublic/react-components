6ebb74982fb57c9894499e32ea986af8d49ef1cc
ben-read <ben.read@zopa.com>
2019-09-23 10:37:51 +0100

    feat(trustpilotlogo): add trustpilot logo component

---
+++ src/components/icons/TrustPilotLogo/TrustPilotLogo.md
@@ -0,0 +1,7 @@
+This component is a wrapper around the svg code for Zopa's logo.
+
+```jsx
+import { TrustPilotLogo } from '@zopauk/react-components';
+
+<TrustPilotLogo width="140px" onLight={true} />;
+```
---
+++ src/components/icons/TrustPilotLogo/TrustPilotLogo.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TrustPilotLogo from './TrustPilotLogo';
+
+describe('<TrustPilotLogo />', () => {
+  it('renders the component with props', () => {
+    const { container } = render(<TrustPilotLogo width="70px" height="50px" />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
