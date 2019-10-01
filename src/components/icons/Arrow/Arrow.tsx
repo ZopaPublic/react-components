c31e91a4370977b4d5e6d25d91637262e44fc874
Lluis Agusti <lluis.agusti@zopa.com>
2019-10-01 14:35:45 +0200

    fix(arrow): remove `id` attrs from the SVG

--- src/components/icons/Arrow/Arrow.tsx
+++ src/components/icons/Arrow/Arrow.tsx
@@ -50,12 +50,11 @@ const Arrow = ({
     {...rest}
   >
     <title>arrow pointing {direction}</title>
-    <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-      <g id="accordion/open" transform="translate(0.000000, -14.000000)" fill={color} fillRule="nonzero">
-        <g id="Group">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g transform="translate(0.000000, -14.000000)" fill={color} fillRule="nonzero">
+        <g>
           <path
             d="M7.63149316,17.3813356 L4.28255792,21.7152282 C3.98915505,22.0949239 3.51345502,22.0949239 3.22005215,21.7152282 C3.07915512,21.5328919 3,21.2855908 3,21.0277282 L3,11.9722718 C3,11.4353009 3.33637072,11 3.75130504,11 C3.95056352,11 4.1416609,11.1024355 4.28255792,11.2847718 L7.63149316,15.6186645 C7.86830411,15.925124 7.92952055,16.0681625 7.96979452,16.2401176 C8.01006849,16.4120727 8.01006849,16.5879273 7.96979452,16.7598824 C7.92952055,16.9318375 7.86830411,17.074876 7.63149316,17.3813356 Z"
-            id="path-1"
             transform="translate(5.500000, 16.500000) rotate(90.000000) translate(-5.500000, -16.500000) "
           />
         </g>
