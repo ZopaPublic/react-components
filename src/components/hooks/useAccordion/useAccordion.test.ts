fc161ddc2ca759c3b63ede26c2be9c4e92467224
thegrinder <paul.szczepkowski@gmail.com>
2019-11-22 10:10:22 +0100

    fix(accordion): fix accordion not opening

--- src/components/hooks/useAccordion/useAccordion.test.ts
+++ src/components/hooks/useAccordion/useAccordion.test.ts
@@ -4,60 +4,48 @@ import useAccordion from './useAccordion';
 
 describe('useAccordion', () => {
   it('should render correct tab and section props', () => {
-    const {
-      result: {
-        current: { getHeaderProps, getSectionProps },
-      },
-    } = renderHook(useAccordion);
+    const { result } = renderHook(useAccordion);
 
-    const header = getHeaderProps('id', 1);
-    const section = getSectionProps('id', 1);
+    const header = result.current.getHeaderProps('id', 1);
+    const section = result.current.getSectionProps('id', 1);
 
     expect(header).toMatchSnapshot();
     expect(section).toMatchSnapshot();
   });
 
   it('should open and close espective sections on header clicks', () => {
-    const {
-      result: {
-        current: { getHeaderProps, isActiveSection },
-      },
-    } = renderHook(useAccordion);
+    const { result } = renderHook(useAccordion);
 
-    const { onClick: firstHeaderClick } = getHeaderProps('id', 1);
+    const { onClick: firstHeaderClick } = result.current.getHeaderProps('one', 1);
 
-    expect(isActiveSection(1)).toEqual(false);
+    expect(result.current.isActiveSection(1)).toEqual(false);
 
     act(() => {
       firstHeaderClick();
     });
 
-    expect(isActiveSection(1)).toEqual(true);
-    const { onClick: secondHeaderClick } = getHeaderProps('id', 2);
-    expect(isActiveSection(2)).toEqual(false);
+    expect(result.current.isActiveSection(1)).toEqual(true);
+    const { onClick: secondHeaderClick } = result.current.getHeaderProps('two', 2);
+    expect(result.current.isActiveSection(2)).toEqual(false);
 
     act(() => {
       secondHeaderClick();
     });
 
-    expect(isActiveSection(2)).toEqual(true);
+    expect(result.current.isActiveSection(2)).toEqual(true);
 
     act(() => {
       secondHeaderClick();
     });
 
-    expect(isActiveSection(2)).toEqual(false);
+    expect(result.current.isActiveSection(2)).toEqual(false);
   });
 
   it('should render matching header and section attributes', () => {
-    const {
-      result: {
-        current: { getHeaderProps, getSectionProps },
-      },
-    } = renderHook(useAccordion);
+    const { result } = renderHook(useAccordion);
 
-    const section = getSectionProps('another-id', 3);
-    const header = getHeaderProps('another-id', 3);
+    const section = result.current.getSectionProps('another-id', 3);
+    const header = result.current.getHeaderProps('another-id', 3);
 
     expect(header.id).toEqual(section['aria-labelledby']);
   });
