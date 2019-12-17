a11900d8a8c93c2cf28ea625e2db9b514ebdee04
Lluis Agusti <lluis.agusti@zopa.com>
2019-12-17 16:30:31 +0100

    feat(expiry-modal): new molecule

---
+++ src/components/molecules/ExpiryModal/ExpiryModal.md
@@ -0,0 +1,45 @@
+### Summary
+
+This is a wrapper around our standard [Modal](./#/Components/Molecules/Modal) ( _which in turn is just a wrapper of [React Modal](https://github.com/reactjs/react-modal)_ 🙃 ) with given copy and styles.
+
+It should be used to warn the user the session is about to expire and give the options to either finish or extend it.
+
+( _This is just an interface, it does not operate any actual user session_ )
+
+### Example
+
+```jsx
+import { ExpiryModal, Modal, Button } from '@zopauk/react-components';
+
+function ExpiryModalDemo() {
+  const [isOpen, update] = React.useState(false);
+  const toggleModal = () => update(prevState => !prevState);
+
+  const onEndSession = () => {
+    window.alert('You are logged out!');
+  };
+
+  const onKeepSession = () => {
+    window.alert('Your session is alive!');
+  };
+
+  return (
+    <>
+      <Button onClick={toggleModal} styling="link">
+        Open Expiry Modal
+      </Button>
+      <ExpiryModal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        onEndSession={onEndSession}
+        onKeepSession={onKeepSession}
+      />
+    </>
+  );
+}
+
+<>
+  <Modal.Styles zIndex={100} />
+  <ExpiryModalDemo />
+</>;
+```
---
+++ src/components/molecules/ExpiryModal/ExpiryModal.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import Modal, { TModalProps } from '../Modal/Modal';
+import Text from '../../atoms/Text/Text';
+import Heading from '../../atoms/Heading/Heading';
+import Button from '../../atoms/Button/Button';
+
+interface IExpiryModalProps extends TModalProps {
+  /**
+   * A callback that fires once the user clicks on 'Log out'
+   **/
+  onEndSession: () => void;
+  /**
+   * A callback that fires once the user clicks on 'Continue'
+   **/
+  onKeepSession: () => void;
+}
+
+const ModalInner = styled.div`
+  padding: 50px 40px 40px;
+  position: relative;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const SessionHint = styled(Text).attrs({
+  forwardedAs: 'p',
+})`
+  margin-bottom: 40px;
+`;
+
+export default function ExpiryModal({ onEndSession, onKeepSession, ...rest }: IExpiryModalProps) {
+  return (
+    <Modal {...rest}>
+      <ModalInner>
+        <Heading as="h2">Your session is about to expire</Heading>
+        <SessionHint>You've gone quiet. What would you like to do?</SessionHint>
+        <ButtonRow>
+          <Button onClick={onEndSession} styling="link">
+            Log out
+          </Button>
+          <Button onClick={onKeepSession}>Continue</Button>
+        </ButtonRow>
+      </ModalInner>
+    </Modal>
+  );
+}
