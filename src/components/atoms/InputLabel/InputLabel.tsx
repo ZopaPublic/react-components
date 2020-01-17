cfbeb6089e357d7d6f07451940b4ade3d037e2c2
David Hewitt <david.hewitt@zopa.com>
2020-01-17 09:17:22 +0000

    fix(inputlabel): remove text component from label

--- src/components/atoms/InputLabel/InputLabel.tsx
+++ src/components/atoms/InputLabel/InputLabel.tsx
@@ -1,21 +1,53 @@
-import React, { ReactNode, FC } from 'react';
+import React, { HTMLAttributes, FC } from 'react';
 import styled from 'styled-components';
-import Text from '../Text/Text';
-import { colors } from '../../../constants/colors';
+import { typography } from '../../../constants/typography';
+import { colors, INeutralColorSpec, ISemanticColorSpec } from '../../../constants/colors';
 
-export interface IInputLabelProps {
+export interface IInputLabelProps extends HTMLAttributes<HTMLLabelElement> {
+  /**
+   * The weight of the rendered text. Avoid the use of `semibold` as for now.
+   */
+  weight?: keyof typeof typography.weights;
+  /**
+   * The size you want to render your text at, currently only `14px` | `16px` and `20px` supported.
+   */
+  size?: keyof typeof typography.sizes.text | 'lead';
+  /**
+   * Whether to add some margin below the rendered text or not. Use it to give meaningful white-space.
+   */
+  mb?: boolean;
+  /**
+   * Accepts a subset of the Zopa brand colors. Same as the ones accepted by `<Heading />`.
+   */
+  color?:
+    | INeutralColorSpec['white']
+    | INeutralColorSpec['nearDark']
+    | INeutralColorSpec['dark']
+    | ISemanticColorSpec['success']
+    | ISemanticColorSpec['error'];
+  /**
+   * Define the associated input identifier
+   */
   htmlFor: string;
-  children: ReactNode;
 }
 
-const InputLabel = styled(Text).attrs({
-  forwardedAs: 'label',
-  weight: 'semibold',
-  color: colors.neutral.dark,
-  size: 'base',
-  mb: false,
-})<IInputLabelProps>`
+const InputLabel = styled.label<IInputLabelProps>`
   display: block;
+  margin: 0;
+  letter-spacing: 0;
+  color: ${({ color = colors.neutral.dark }) => color};
+
+  ${({ mb = false }) =>
+    mb &&
+    `
+    display: block;
+    margin-bottom:   24px
+  `};
+
+  line-height: ${typography.lineHeights.text};
+  font-family: ${typography.primary};
+  font-weight: ${typography.weights.semibold};
+  font-size: ${({ size = 'base' }) => (size === 'lead' ? typography.sizes.heading.h4 : typography.sizes.text[size])};
 `;
 
 // TODO: Styleguidist to be able to locate styled components. See #147.
