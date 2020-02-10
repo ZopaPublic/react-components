fb5cd8982f951eb5c02604b8b88baf956c517894
thegrinder <paul.szczepkowski@gmail.com>
2020-02-10 09:21:15 +0000

    fix(formdropdownfield): forward ref

--- src/components/organisms/Form/FormDropdownField/FormDropdownField.tsx
+++ src/components/organisms/Form/FormDropdownField/FormDropdownField.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ChangeEvent } from 'react';
+import React, { forwardRef, ChangeEvent } from 'react';
 import DropdownField, { IDropdownFieldProps } from '../../../molecules/DropdownField/DropdownField';
 import { useFieldContext } from '../hooks';
 
@@ -6,7 +6,7 @@ interface IFormDropdownFieldProps extends IDropdownFieldProps {
   name: string;
 }
 
-const FormDropdownField: FC<IFormDropdownFieldProps> = ({ name, ...rest }) => {
+const FormDropdownField = forwardRef<HTMLSelectElement, IFormDropdownFieldProps>(({ name, ...rest }, ref) => {
   const { error, touched, onChange, onBlur } = useFieldContext(name);
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -20,9 +20,10 @@ const FormDropdownField: FC<IFormDropdownFieldProps> = ({ name, ...rest }) => {
       errorMessage={touched && error ? error : ''}
       onChange={handleChange}
       onBlur={onBlur}
+      ref={ref}
       {...rest}
     />
   );
-};
+});
 
 export default FormDropdownField;
