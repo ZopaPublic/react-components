550625c5fb0e8ef541f6684ee8ec4fd72a91bddb
Timo Laak <1674055+tlaak@users.noreply.github.com>
2020-04-28 18:23:45 +0100

    improvement(accordion): remove required aria-label prop

--- src/components/organisms/Accordion/Accordion/Accordion.test.tsx
+++ src/components/organisms/Accordion/Accordion/Accordion.test.tsx
@@ -3,7 +3,13 @@ import { render } from '@testing-library/react';
 import { Accordion } from '..';
 
 describe('<Accordion />', () => {
-  it('should render aria-label', () => {
+  it('should render standard attributes but not require aria-label', () => {
+    const title = 'test-accordion';
+    const { queryByTitle } = render(<Accordion title={title} />);
+    expect(queryByTitle(title)).toBeTruthy();
+  });
+
+  it('should render aria-label in case it is passed', () => {
     const ariaLabel = 'test-accordion';
     const { queryByLabelText } = render(<Accordion aria-label={ariaLabel} />);
     expect(queryByLabelText(ariaLabel)).toBeTruthy();
