47ed95b650fdf8b8371dd34fee2430db50e34db7
David Fernandez <dfvalero@gmail.com>
2020-05-07 20:08:05 +0200

    style(zopafooter): footer rebranded

--- src/components/molecules/ZopaFooter/ZopaFooter.test.tsx
+++ src/components/molecules/ZopaFooter/ZopaFooter.test.tsx
@@ -17,18 +17,9 @@ describe('<ZopaFooter />', () => {
     date.finishMocking();
   });
 
-  it('renders the component with default props', async () => {
-    const { container } = render(<ZopaFooter />);
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it('renders the component with a legalOnly prop', async () => {
-    const { container } = render(<ZopaFooter legalOnly />);
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
   it('renders the component with no a11y violations', async () => {
     const { container } = render(<ZopaFooter />);
+    expect(container.firstChild).toMatchSnapshot();
     const results = await axe(container.innerHTML);
     expect(results).toHaveNoViolations();
   });
---
+++ src/components/molecules/ZopaFooter/styles/Footer.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { spacing } from '../../../../constants/spacing';
+
+export const Footer = styled.footer`
+  margin-bottom: ${spacing[9]};
+`;
