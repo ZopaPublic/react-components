a0e86f0748b3e09c13741666dd56e2e3168f381d
ben-read <ben.read@zopa.com>
2020-05-07 09:21:24 +0100

    fix(cardtext): flex on card text for uniform height card series

--- src/components/organisms/Card/CardContent/CardContent.tsx
+++ src/components/organisms/Card/CardContent/CardContent.tsx
@@ -4,9 +4,6 @@ const CardContent = styled.div`
   background-color: white;
   padding: 16px;
   flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
 `;
 
 export default CardContent;
