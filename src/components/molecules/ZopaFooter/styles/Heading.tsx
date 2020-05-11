cfec68d44798fb802ac8a1280a7967f8b4a8b6ab
David Fernandez <dfvalero@gmail.com>
2020-05-11 11:04:03 +0200

    refactor(footer): fix yarn compile error, eslint warning

--- src/components/molecules/ZopaFooter/styles/Heading.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import ZopaHeading from '../../../atoms/Heading/Heading';
-
-export const Heading = styled(ZopaHeading).attrs({ as: 'h3', size: 'h6' })`
-  text-transform: uppercase;
-`;
