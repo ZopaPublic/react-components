4df55e505190184d4dee6736f9e1eebd9a743cdf
thegrinder <paul.szczepkowski@gmail.com>
2020-05-13 13:25:01 +0100

    refactor(dropdownfiltered): refactor it to functional component

--- src/components/molecules/DropdownFiltered/DropdownFiltered.tsx
+++ src/components/molecules/DropdownFiltered/DropdownFiltered.tsx
@@ -36,121 +36,114 @@ const FieldError = styled(ErrorMessage)`
   margin-top: 5px;
 `;
 
-export default class DropdownFiltered extends React.PureComponent<IDropdownFilteredProps> {
-  public render() {
-    return (
-      <SizedContainer size={this.props.inputSize}>
-        <Downshift itemToString={item => (item ? item.value : '')} {...this.props}>
-          {this.dropdownContent}
-        </Downshift>
-      </SizedContainer>
-    );
-  }
+const DropdownFiltered = (props: IDropdownFilteredProps) => {
+  const {
+    errorMessage,
+    items = [],
+    label,
+    optionsListMaxHeight,
+    name,
+    labelProps,
+    isValid,
+    disabled,
+    onChange,
+    onFocus,
+    inputSize,
+    ...rest
+  } = props;
 
-  private searchMatch = (itemValue: string, inputValue: string) =>
+  const searchMatch = (itemValue: string, inputValue: string) =>
     itemValue && itemValue.toLowerCase().includes(inputValue.toLowerCase());
 
-  private dropdownContent = (options: ControllerStateAndHelpers<IDropdownItem>) => {
-    const {
-      clearSelection,
-      getInputProps,
-      getItemProps,
-      getLabelProps,
-      getMenuProps,
-      highlightedIndex,
-      inputValue,
-      isOpen,
-      openMenu,
-      closeMenu,
-      selectedItem,
-    } = options;
+  return (
+    <SizedContainer size={inputSize}>
+      <Downshift itemToString={item => (item ? item.value : '')} {...props}>
+        {({
+          clearSelection,
+          getInputProps,
+          getItemProps,
+          getLabelProps,
+          getMenuProps,
+          highlightedIndex,
+          inputValue,
+          isOpen,
+          openMenu,
+          closeMenu,
+          selectedItem,
+        }: ControllerStateAndHelpers<IDropdownItem>) => {
+          const filteredResults = items.filter(({ value }) => searchMatch(value, inputValue || ''));
 
-    const {
-      errorMessage,
-      items = [],
-      label,
-      optionsListMaxHeight,
-      name,
-      labelProps,
-      isValid,
-      disabled,
-      onChange,
-      onFocus,
-      ...rest
-    } = this.props;
-    if (!name) {
-      throw Error('Name must be set in inputProps. Check the docs.');
-    }
-
-    const filteredResults = items.filter(({ value }) => this.searchMatch(value, inputValue || ''));
-
-    const showError = errorMessage && !isOpen;
-
-    return (
-      <div>
-        {label && (
-          <InputLabel {...getLabelProps({ ...labelProps })} htmlFor={`text-id-${name}`}>
-            {label}
-          </InputLabel>
-        )}
-        <SearchInputWrap>
-          <SearchInput
-            {...(getInputProps as any)({
-              // Types doesn't allow custom data-* attrs
-              onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-                if (e.target.value === '') {
-                  clearSelection();
-                }
-              },
-              onFocus: () => {
-                openMenu();
-              },
-              ...rest,
-            })}
-            name={name}
-            id={`text-id-${name}`}
-            isValid={isValid}
-            hasError={showError}
-            isOpen={isOpen}
-            disabled={disabled}
-          />
-          <SearchArrowWrap>
-            <Icon
-              variant={faChevronDown}
-              rotation={isOpen ? 180 : undefined}
-              color={disabled ? colors.greyLight : colors.actionPlain}
-              onClick={() => {
-                if (!disabled) {
-                  if (isOpen) {
-                    closeMenu();
-                  } else {
-                    clearSelection(() => {
+          const showError = errorMessage && !isOpen;
+          return (
+            <div>
+              {label && (
+                <InputLabel {...getLabelProps({ ...labelProps })} htmlFor={`text-id-${name}`}>
+                  {label}
+                </InputLabel>
+              )}
+              <SearchInputWrap>
+                <SearchInput
+                  {...(getInputProps as any)({
+                    // Types doesn't allow custom data-* attrs
+                    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+                      if (e.target.value === '') {
+                        clearSelection();
+                      }
+                    },
+                    onFocus: () => {
                       openMenu();
-                    });
-                  }
-                }
-              }}
-              aria-label={isOpen ? 'close.menu' : 'open.menu'}
-            />
-          </SearchArrowWrap>
-          {isOpen && (
-            <Options {...getMenuProps()} optionsListMaxHeight={optionsListMaxHeight}>
-              {filteredResults.map((item, index) => (
-                <Option
-                  {...getItemProps({ index, item })}
-                  key={item.value}
-                  highLighted={highlightedIndex === index}
-                  selected={selectedItem === item}
+                    },
+                    ...rest,
+                  })}
+                  name={name}
+                  id={`text-id-${name}`}
+                  isValid={isValid}
+                  hasError={showError}
+                  isOpen={isOpen}
                   disabled={disabled}
-                >
-                  {item.value}
-                </Option>
-              ))}
-            </Options>
-          )}
-        </SearchInputWrap>
-        {showError && <FieldError data-automation={`ZA.error-${name}`}>{errorMessage}</FieldError>}
-      </div>
-    );
-  };
-}
+                />
+                <SearchArrowWrap>
+                  <Icon
+                    variant={faChevronDown}
+                    rotation={isOpen ? 180 : undefined}
+                    color={disabled ? colors.greyLight : colors.actionPlain}
+                    onClick={() => {
+                      if (!disabled) {
+                        if (isOpen) {
+                          closeMenu();
+                        } else {
+                          clearSelection(() => {
+                            openMenu();
+                          });
+                        }
+                      }
+                    }}
+                    aria-label={isOpen ? 'close.menu' : 'open.menu'}
+                  />
+                </SearchArrowWrap>
+                {isOpen && (
+                  <Options {...getMenuProps()} optionsListMaxHeight={optionsListMaxHeight}>
+                    {filteredResults.map((item, index) => (
+                      <Option
+                        {...getItemProps({ index, item })}
+                        key={item.value}
+                        highLighted={highlightedIndex === index}
+                        selected={selectedItem === item}
+                        disabled={disabled}
+                      >
+                        {item.value}
+                      </Option>
+                    ))}
+                  </Options>
+                )}
+              </SearchInputWrap>
+              {showError && <FieldError data-automation={`ZA.error-${name}`}>{errorMessage}</FieldError>}
+            </div>
+          );
+        }}
+      </Downshift>
+    </SizedContainer>
+  );
+};
+
+export default DropdownFiltered;
