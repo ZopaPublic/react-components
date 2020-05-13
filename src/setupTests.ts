31cd46fe296a4072180630b05d0c3027cd73ef56
Gerard Brull <gerard.brull@zopa.com>
2020-05-13 10:32:06 +0200

    chore(node): deprecate node v10

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,11 +1,3 @@
 import 'jest-styled-components';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-axe/extend-expect';
-
-// polyfill globalThis for older nodes
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-globalThis = global;
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { JSDOM } = require('jsdom');
-new JSDOM('', { runScripts: 'dangerously' });
