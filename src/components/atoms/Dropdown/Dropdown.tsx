b621899600d49670bc9fbceb2654261ec4e0d71e
thegrinder <paul.szczepkowski@gmail.com>
2020-05-18 13:04:24 +0100

    fix(dropdown): change styling

--- src/components/atoms/Dropdown/Dropdown.tsx
+++ src/components/atoms/Dropdown/Dropdown.tsx
@@ -1,11 +1,8 @@
 import React, { forwardRef } from 'react';
 import styled from 'styled-components';
 import { colors } from '../../../constants/colors';
-import chevronDown from '../../../content/images/chevron-down.svg';
 import { getBorderColorByStatus } from '../../../helpers/utils';
 import { typography } from '../../../constants/typography';
-
-export const DEFAULT_COLOR = colors.greyLight;
 
 export interface IDropdownProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   /**
@@ -22,20 +19,23 @@ export const Option = styled.option``;
 
 const StyledDropdown = styled.select<IDropdownProps>`
   appearance: none;
-  background: transparent url(${chevronDown}) no-repeat calc(100% - 13px) center;
-  background-size: 13px;
-  border: 2px solid ${getBorderColorByStatus};
-  border-radius: 4px;
-  padding: 8px 32px 8px 16px;
-  font-weight: ${typography.weights.semiBold};
-  height: 46px;
   min-width: 100px;
   user-select: none;
+  background: transparent;
+  padding-right: 32px;
+  padding-left: 16px;
+  height: 50px;
+  border-radius: 4px;
+  font-weight: ${typography.weights.regular};
+  border: 1px solid ${getBorderColorByStatus};
+  box-shadow: 0 0 4px 0 transparent;
+  transition-property: border, box-shadow;
+  transition: 0.2s ease-in-out;
 
   &:focus {
     outline-width: 0;
-    border: 2px solid ${colors.actionPlain};
-    transition: border 0.2s;
+    border: 1px solid ${colors.brand};
+    box-shadow: 0 0 4px 0 ${colors.brand};
   }
 `;
 
