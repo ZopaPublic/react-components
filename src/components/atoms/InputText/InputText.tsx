dd13d1b65311ad2cfde7ef045a5e0227b88d3190
thegrinder <paul.szczepkowski@gmail.com>
2020-06-16 12:33:52 +0100

    feat(inputtext): add startIcon and endIcon to InputText

--- src/components/atoms/InputText/InputText.tsx
+++ src/components/atoms/InputText/InputText.tsx
@@ -1,16 +1,51 @@
 import React, { forwardRef } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { colors, typography } from '../../../constants';
 import { getBorderColorByStatus, getInputTextColor } from '../../../helpers/utils';
 import { InputProps } from '../../types';
+
+type IconWrapperProps = {
+  start?: boolean;
+};
+
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
+const IconWrapper = styled.span<IconWrapperProps>`
+  position: absolute;
+  top: 1px;
+  bottom: 1px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  color: ${colors.grey};
+  ${({ start }) =>
+    start
+      ? css`
+          left: 1px;
+          border-top-left-radius: 8px;
+          border-bottom-left-radius: 8px;
+        `
+      : css`
+          right: 1px;
+          border-top-right-radius: 8px;
+          border-bottom-right-radius: 8px;
+        `}
+  background-color: ${colors.greyLighter};
+  border-top-left-radius
+`;
 
 const Input = styled.input<InputProps>`
   width: 100%;
   -webkit-appearance: none;
   outline: none;
   border-radius: 8px;
-  padding: 0 16px;
   height: 50px;
+  padding: 0 16px;
+  padding-left: ${({ startIcon }) => !!startIcon && '60px'};
+  padding-right: ${({ endIcon }) => !!endIcon && '60px'};
   font-size: ${typography.sizes.text.body};
   font-weight: ${typography.weights.regular};
   color: ${getInputTextColor}
@@ -37,8 +72,12 @@ const Input = styled.input<InputProps>`
   }
 `;
 
-const InputText = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  return <Input {...props} ref={ref} />;
-});
+const InputText = forwardRef<HTMLInputElement, InputProps>(({ startIcon, endIcon, className, ...rest }, ref) => (
+  <InputWrapper className={className}>
+    {startIcon && <IconWrapper start>{startIcon}</IconWrapper>}
+    <Input startIcon={startIcon} endIcon={endIcon} {...rest} ref={ref} />
+    {endIcon && <IconWrapper start={false}>{endIcon}</IconWrapper>}
+  </InputWrapper>
+));
 
 export default InputText;
