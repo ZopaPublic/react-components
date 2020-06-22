0029ed6d1121d207cd2e11316fb6e99b2de4bdd3
thegrinder <paul.szczepkowski@gmail.com>
2020-06-22 19:12:28 +0100

    fix(dropdownfiltered): handle cursor states

--- src/components/molecules/DropdownFiltered/SearchInput.ts
+++ src/components/molecules/DropdownFiltered/SearchInput.ts
@@ -8,12 +8,20 @@ export interface SearchInputProps {
 }
 
 export const SearchInput = styled(InputText)<SearchInputProps & InputProps>`
-  ${({ hasError }) => !hasError && 'margin-bottom: 0'};
-  ${({ isOpen }) =>
-    isOpen &&
-    css`
-      box-shadow: 0 0 4px 0 ${colors.brand};
-      border-radius: 8px 8px 0 0;
-      border-color: ${colors.brand};
-    `};
+  input {
+    ${({ hasError }) => !hasError && 'margin-bottom: 0'};
+    ${({ isOpen }) =>
+      isOpen &&
+      css`
+        box-shadow: 0 0 4px 0 ${colors.brand};
+        border-radius: 8px 8px 0 0;
+        border-color: ${colors.brand};
+      `};
+  }
+  input + span {
+    cursor: pointer;
+  }
+  input:disabled + span {
+    cursor: not-allowed;
+  }
 `;
