e175f467473a26c712c13317be6ae038ff786703
raff-r <hello@raffr.co.uk>
2020-07-22 12:35:56 +0100

    fix(radio): remove conosle log

--- src/components/organisms/Form/FormRadioGroupField/FormRadioGroupField.tsx
+++ src/components/organisms/Form/FormRadioGroupField/FormRadioGroupField.tsx
@@ -7,7 +7,6 @@ export type FormRadioGroupFieldProps = Pick<FieldHookConfig<string>, 'validate' 
 
 const FormRadioGroupField = ({ name, validate, ...rest }: FormRadioGroupFieldProps) => {
   const [{ value }, { error, touched }, { setValue, setTouched }] = useField<string>({ name, validate });
-  console.log(rest);
   return (
     <RadioGroupField
       onChange={setValue}
