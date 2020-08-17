b2cb85897722f37607fd56d9ad03a6e5bc23cd0c
Matt Revell <mattrevell82@me.com>
2020-08-17 15:11:28 +0100

    test(alert): fixes up the naming and adds the missing banner tests

--- src/components/molecules/Banner/Banner.test.tsx
+++ src/components/molecules/Banner/Banner.test.tsx
@@ -1,11 +1,11 @@
 import { axe } from 'jest-axe';
 import { render } from '@testing-library/react';
-import Notification from './Notification';
+import Banner from './Banner';
 import React from 'react';
 
-describe('Notification', () => {
+describe('Banner', () => {
   it('renders the component with no props and no a11y violations', async () => {
-    const { container } = render(<Notification>Notification Content</Notification>);
+    const { container } = render(<Banner>Banner Content</Banner>);
     expect(container.firstChild).toMatchSnapshot();
     const results = await axe(container.innerHTML);
     expect(results).toHaveNoViolations();
