57235e13a5a837517c36d48cb2fdd5ec87c707e1
Gerard Brull <gerard.brull@zopa.com>
2020-08-19 13:41:53 +0200

    fix(tooltip): upgrade tippy.js library

--- src/components/molecules/Tooltip/Tooltip.tsx
+++ src/components/molecules/Tooltip/Tooltip.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Tippy, { TippyProps } from '@tippy.js/react';
+import Tippy, { TippyProps } from '@tippyjs/react';
 import TooltipStyles from './TooltipStyles/TooltipStyles';
 
 Tooltip.Styles = TooltipStyles;
 
 export default function Tooltip({ children, ...config }: Omit<TippyProps, 'animation' | 'theme' | 'flipOnUpdate'>) {
   return (
-    <Tippy allowHTML={false} placement="top" animation="shift-toward" flipOnUpdate={true} inertia={true} {...config}>
+    <Tippy allowHTML={false} placement="top" animation="shift-toward" inertia={true} {...config}>
       {children}
     </Tippy>
   );
