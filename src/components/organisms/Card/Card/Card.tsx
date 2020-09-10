76d7ba5e1aa03d69a607813c8b123b39efc5e268
Kate Beavis <katebeavis84@gmail.com>
2020-09-10 14:40:06 +0100

    feat(card): add new styles

--- src/components/organisms/Card/Card/Card.tsx
+++ src/components/organisms/Card/Card/Card.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { colors, typography } from '../../../../constants';
 import { CardImageContainer } from '../CardImage/CardImage';
+import CardContent from '../CardContent/CardContent';
 import CardHeading from '../CardHeading/CardHeading';
 import CardText from '../CardText/CardText';
 
 export type CardLayout = 'horizontal' | 'vertical';
-export type CardStyling = 'primary' | 'secondary';
+export type CardStyling = 'primary' | 'secondary' | 'brand';
 
 export interface CardProps {
   /**
@@ -33,11 +34,31 @@ const layoutImageStyles: Record<CardLayout, string> = {
 const headingSizes: Record<CardStyling, string> = {
   secondary: typography.sizes.heading.h6,
   primary: typography.sizes.heading.h5,
+  brand: typography.sizes.heading.h6,
 };
 
 const textSizes: Record<CardStyling, string> = {
   secondary: typography.sizes.text.small,
   primary: typography.sizes.text.body,
+  brand: typography.sizes.text.small,
+};
+
+const boxShadowStyle: Record<CardStyling, string> = {
+  primary: `0 1px 0 0 ${colors.greyLight}`,
+  secondary: `0 1px 0 0 ${colors.greyLight}`,
+  brand: `none`,
+};
+
+const borderStyle: Record<CardStyling, string> = {
+  primary: `1px solid ${colors.greyLighter}`,
+  secondary: `1px solid ${colors.greyLighter}`,
+  brand: `1px solid ${colors.brand}`,
+};
+
+const backgroundStyle: Record<CardStyling, string> = {
+  primary: `${colors.white};`,
+  secondary: `${colors.white};`,
+  brand: `${colors.brandLight};`,
 };
 
 const Card = styled.div<CardProps>`
@@ -46,9 +67,9 @@ const Card = styled.div<CardProps>`
   display: flex;
   overflow: hidden;
   background-color: ${colors.white};
-  border: 1px solid ${colors.greyLighter};
+  border: ${({ styling = 'secondary' }) => borderStyle[styling]};
   border-radius: 12px;
-  box-shadow: 0 1px 0 0 ${colors.greyLight};
+  box-shadow: ${({ styling = 'secondary' }) => boxShadowStyle[styling]};
   flex-direction: ${({ layout = 'vertical' }) => layoutDirections[layout]};
   & > ${CardImageContainer} {
     flex: 0 1 ${({ layout = 'vertical' }) => layoutImageStyles[layout]};
@@ -58,6 +79,9 @@ const Card = styled.div<CardProps>`
   }
   & ${CardText} {
     font-size: ${({ styling = 'secondary' }) => textSizes[styling]};
+  }
+  & ${CardContent} {
+    background-color: ${({ styling = 'secondary' }) => backgroundStyle[styling]};
   }
 `;
 
