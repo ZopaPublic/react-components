d262835e8ba3aee372f706c98fc9a731a04012a0
Kate Beavis <katebeavis84@gmail.com>
2020-09-11 15:26:36 +0100

    feat(action card): add action card with loading icons

--- src/components/molecules/ActionCard/ActionCard.md
+++ src/components/molecules/ActionCard/ActionCard.md
@@ -5,12 +5,42 @@ This is a wrapper around [Card](./#/Components/Organisms/Card).
 ### Example
 
 ```tsx
-import { ActionCard, CardImage, FlexContainer, FlexRow, FlexCol, Text } from '@zopauk/react-components';
+import { ActionCard, CardImage, FlexContainer, FlexRow, FlexCol, Text, Spinner, Icon } from '@zopauk/react-components';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+const ActionCardIcon = (selected, cardId, loading) => {
+  return loading && selected === cardId ? (
+    <Spinner size="small" />
+  ) : (
+    <Icon variant={faChevronRight} color={colors.grey} />
+  );
+};
+
+const handleClick = (id) => {
+  alert(`The card with id ${id} has been clicked`);
+};
 
 <FlexContainer>
   <FlexRow>
     <FlexCol xs={4}>
-      <Actioncard />
+      <ActionCard handleOnClick={handleClick} id="option-1" loading={true} handleClick={handleClick}>
+        <Text as="p" weight="bold" className="mb-1">
+          Option 1
+        </Text>
+        <Text as="p" size="small">
+          I would like to select a option 1
+        </Text>
+      </ActionCard>
+    </FlexCol>
+    <FlexCol xs={4}>
+      <ActionCard handleOnClick={handleClick} id="option-2" loading={true} handleClick={handleClick}>
+        <Text as="p" weight="bold" className="mb-1">
+          Option 2
+        </Text>
+        <Text as="p" size="small">
+          I would like to select option 2
+        </Text>
+      </ActionCard>
     </FlexCol>
   </FlexRow>
 </FlexContainer>;
--- src/components/molecules/ActionCard/ActionCard.tsx
+++ src/components/molecules/ActionCard/ActionCard.tsx
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import Card from '../../organisms/Card';
+import { colors } from '../../../constants';
+import Spinner from '../../atoms/Spinner/Spinner';
+import Icon from '../../atoms/Icon/Icon';
+import { faChevronRight, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
-const ActionCard = () => {
-  return <Card>hi</Card>;
+interface ActionCardProps {
+  children: any;
+  id: string;
+  icon: IconDefinition;
+  loading: boolean;
+  handleClick: (id: string) => void;
+}
+
+const ActionCardIcon = (selected: string, cardId: string, icon: IconDefinition, loading?: boolean) => {
+  return loading && selected === cardId ? <Spinner size="small" /> : <Icon variant={icon} color={colors.grey} />;
+};
+
+const StyledActionCard = styled(Card)`
+  /* &:hover {
+    cursor: pointer;
+  } */
+`;
+
+const StyledActionCardContent = styled(Card.Content)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+
+  &:hover {
+    cursor: pointer;
+    svg {
+      color: ${colors.greyDark};
+    }
+    background-color: ${colors.greyLightest};
+  }
+`;
+
+const ActionCard: FC<ActionCardProps> = ({ children, id, icon = faChevronRight, loading, handleClick }) => {
+  const [selectedId, setSelectedId] = useState<string>('');
+  const handleOnClick = () => {
+    setSelectedId(id);
+    handleClick(id);
+  };
+  return (
+    <StyledActionCard onClick={handleOnClick}>
+      <StyledActionCardContent>
+        <div>{children}</div>
+        {ActionCardIcon(selectedId, id, icon, loading)}
+      </StyledActionCardContent>
+    </StyledActionCard>
+  );
 };
 
 export default ActionCard;
