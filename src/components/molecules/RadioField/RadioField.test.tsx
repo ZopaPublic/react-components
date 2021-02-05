90ef09b974f3a2713e8b7ff75a632026245242c9
Tom Groombridge <tom.groombridge@zopa.com>
2021-02-05 14:12:08 +0000

    fix(radiofield): fix test snapshots

--- src/components/molecules/RadioField/RadioField.test.tsx
+++ src/components/molecules/RadioField/RadioField.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import axe from '../../../../axe-helper';
 import InputCheckbox from './RadioField';
 
-xdescribe('<RadioField />', () => {
+describe('<RadioField />', () => {
   it('renders the component with props with no a11y violations', async () => {
     const { container } = render(<InputCheckbox label="hello" name="test1" value="1" />);
     const results = await axe(container.innerHTML);
