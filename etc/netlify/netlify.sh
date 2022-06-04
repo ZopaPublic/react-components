76aae3cfe1a82c9b5ffb8a2f7d5d42523cc636af
matthew.revell <matthew.revell@zopa.com>
2022-06-04 19:55:42 +0100

    chore(core): update pnpm

--- etc/netlify/netlify.sh
+++ etc/netlify/netlify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-npx pnpm@6.32.10 install -r --store-dir=node_modules/.pnpm-store
-npx pnpm@6.32.10 build
+npx pnpm@6.32.19 install -r --store-dir=node_modules/.pnpm-store
+npx pnpm@6.32.19 build
