552fb04271b343b36642804ddbee24c12f354a7e
Gabor Halasz <gabor.halasz@zopa.com>
2022-06-13 13:30:05 +0100

    fix(zopafooter): theming bug

--- src/components/molecules/ZopaFooter/styles/LegalBlock.tsx
+++ src/components/molecules/ZopaFooter/styles/LegalBlock.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 import FlexCol from '../../../layout/FlexCol/FlexCol';
+import Text from '../../../atoms/Text/Text';
 import { grid } from '../../../../constants';
 
 export const LegalBlock = styled(FlexCol)`
   order: 3;
+  p {
+    font-size: ${({ theme }) => theme.footer.legalBlock.fontSize};
+    color: ${({ theme }) => theme.footer.legalBlock.color};
+  }
 
   @media (min-width: ${grid.breakpoints.l}px) {
     order: 2;
--- src/components/styles/Theme.tsx
+++ src/components/styles/Theme.tsx
@@ -59,11 +59,16 @@ interface ErrorMessageTheme {
 
 interface FooterTheme {
   bgColor: string;
+  className: string;
   showFooterLinks: boolean;
   showLogoBlock: boolean;
   showSocialBlock: boolean;
   showLegalBlock: boolean;
-  isLegalBlockFullWidth: boolean;
+  legalBlock: {
+    isFullWidth: boolean;
+    color: string;
+    fontSize: string;
+  };
 }
 
 interface InputTheme {
@@ -330,11 +335,16 @@ export const zopaTheme: AppTheme = {
   },
   footer: {
     bgColor: colors.white,
+    className: 'pb-9 pt-10 l:pt-11',
     showFooterLinks: true,
     showLogoBlock: true,
     showSocialBlock: true,
     showLegalBlock: true,
-    isLegalBlockFullWidth: false,
+    legalBlock: {
+      isFullWidth: false,
+      color: colors.greyDark,
+      fontSize: typography.sizes.text.small,
+    },
   },
   input: {
     color: colors.greyDark,
