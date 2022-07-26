d5ee279349cdad2158637ddcdd4ec81c3d1d930e
David Hewitt <david.hewitt@zopa.com>
2022-07-26 13:54:52 +0100

    Remove unused vars

--- src/components/organisms/Card/Card/Card.tsx
+++ src/components/organisms/Card/Card/Card.tsx
@@ -32,46 +32,6 @@ const layoutImageStyles: Record<CardLayout, string> = {
   vertical: '200px',
 };
 
-const headingSizes: Record<CardStyling, string> = {
-  secondary: typography.sizes.heading.h6,
-  primary: typography.sizes.heading.h5,
-  brand: typography.sizes.heading.h6,
-  action: typography.sizes.heading.h6,
-  info: typography.sizes.heading.h6,
-};
-
-const textSizes: Record<CardStyling, string> = {
-  secondary: typography.sizes.text.small,
-  primary: typography.sizes.text.body,
-  brand: typography.sizes.text.small,
-  action: typography.sizes.text.small,
-  info: typography.sizes.text.small,
-};
-
-const boxShadowStyle: Record<CardStyling, string> = {
-  primary: `0 1px 0 0 ${colors.greyLight}`,
-  secondary: `0 1px 0 0 ${colors.greyLight}`,
-  brand: `none`,
-  action: `none`,
-  info: `none`,
-};
-
-const borderStyle: Record<CardStyling, string> = {
-  primary: `1px solid ${colors.greyLighter}`,
-  secondary: `1px solid ${colors.greyLighter}`,
-  brand: `1px solid ${colors.brand}`,
-  action: `1px solid ${colors.greyLighter}`,
-  info: `1px solid ${colors.greyLightest}`,
-};
-
-const backgroundStyle: Record<CardStyling, string> = {
-  primary: `${colors.white};`,
-  secondary: `${colors.white};`,
-  brand: `${colors.brandLight};`,
-  action: `${colors.white};`,
-  info: `${colors.greyLightest}`,
-};
-
 const StyledCard = styled.div<CardProps & { theme: AppTheme }>`
   height: 100%;
   width: 100%;
