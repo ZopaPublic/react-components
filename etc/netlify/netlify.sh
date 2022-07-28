af6fd1a3264ac4f7d9d929c52d0feacef249dabb
Matt Revell <mattrevell82@me.com>
2022-07-28 08:40:24 +0100

    chore(pnpm): use pnpm 7.6.0 in netlify
    See: https://github.com/pnpm/pnpm/releases/tag/v7.6.0

--- etc/netlify/netlify.sh
+++ etc/netlify/netlify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-npx pnpm@7.5.0 install -r --store-dir=node_modules/.pnpm-store
-npx pnpm@7.5.0 build
+npx pnpm@7.6.0 install -r --store-dir=node_modules/.pnpm-store
+npx pnpm@7.6.0 build
