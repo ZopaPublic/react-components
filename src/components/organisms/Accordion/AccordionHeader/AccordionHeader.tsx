652329ab63a96ce1298c8e153b550b1a23c64823
Charlotte E Walker <charlotttee.walker@zopa.com>
2022-12-09 15:06:16 +0000

    fix: changed heading accordion to be heading rather than span for accessibility improvements

--- src/components/organisms/Accordion/AccordionHeader/AccordionHeader.tsx
+++ src/components/organisms/Accordion/AccordionHeader/AccordionHeader.tsx
@@ -1,7 +1,7 @@
 import React, { HTMLAttributes } from 'react';
 import styled from 'styled-components';
 import { colors, spacing } from '../../../../constants';
-import Text from '../../../atoms/Text/Text';
+import Heading from '../../../atoms/Heading/Heading';
 import { useAccordionContext } from '../context';
 
 export interface AccordionHeader extends Omit<HTMLAttributes<HTMLButtonElement>, 'onClick'> {
@@ -30,10 +30,6 @@ const TitleContainer = styled.div`
   justify-content: space-between;
   position: relative;
   padding: ${spacing[4]} 43px ${spacing[4]} ${spacing[4]};
-`;
-
-const Title = styled(Text)`
-  color: ${colors.greyDarkest};
 `;
 
 const Cross = styled.span<{ active: boolean }>`
@@ -76,9 +72,9 @@ const AccordionHeader = ({ children, id, index, textSize = 'body', onClick, ...r
   return (
     <StyledButton type="button" ref={ref} onClick={handleClick} {...headerPropsRest} {...rest}>
       <TitleContainer>
-        <Title weight="bold" size={textSize}>
+        <Heading as="h3" className="my-0" color={colors.greyDarkest} size="h6">
           {children}
-        </Title>
+        </Heading>
         <Cross active={isActiveSection(index)} />
       </TitleContainer>
     </StyledButton>
