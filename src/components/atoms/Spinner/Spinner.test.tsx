70eb5114c31ea2e33d6c7644b6a2af0e3e70ca52
Charlotte E Walker <charlielizzy@me.com>
2023-02-21 12:12:39 +0000

    feat: added three dots spinner

--- src/components/atoms/Spinner/Spinner.test.tsx
+++ src/components/atoms/Spinner/Spinner.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axe from '../../../../axe-helper';
 import { render } from '@testing-library/react';
 import Spinner from './Spinner';
+import ThreeDotsSpinner from './ThreeDotsSpinner';
 
 describe('<Spinner />', () => {
   it('renders a spinner', async () => {
@@ -19,5 +20,14 @@ describe('<Spinner />', () => {
   it('renders negative', () => {
     const { container } = render(<Spinner negative />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+});
+
+describe('<ThreeDotsSpinner />', () => {
+  it('renders three dots spinner', async () => {
+    const { container } = render(<ThreeDotsSpinner />);
+    expect(container.firstChild).toMatchSnapshot();
+    const results = await axe(container.innerHTML);
+    expect(results).toHaveNoViolations();
   });
 });
