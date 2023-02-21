54e3a73d031b2e0288a297b1b76ead5cba5853dc
Charlotte E Walker <charlielizzy@me.com>
2023-02-21 13:46:44 +0000

    test: delete duplicate test for three dots spinner

--- src/components/atoms/Spinner/Spinner.test.tsx
+++ src/components/atoms/Spinner/Spinner.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import axe from '../../../../axe-helper';
 import { render } from '@testing-library/react';
 import Spinner from './Spinner';
-import ThreeDotsSpinner from './ThreeDotsSpinner';
 
 describe('<Spinner />', () => {
   it('renders a spinner', async () => {
@@ -20,14 +19,5 @@ describe('<Spinner />', () => {
   it('renders negative', () => {
     const { container } = render(<Spinner negative />);
     expect(container.firstChild).toMatchSnapshot();
-  });
-});
-
-describe('<ThreeDotsSpinner />', () => {
-  it('renders three dots spinner', async () => {
-    const { container } = render(<ThreeDotsSpinner />);
-    expect(container).toMatchSnapshot();
-    const results = await axe(container.innerHTML);
-    expect(results).toHaveNoViolations();
   });
 });
