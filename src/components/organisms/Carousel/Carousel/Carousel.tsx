f5ab852513d92132ded7e2fbd75db85b5c7312cc
OlenaKashuba <amely.jazz@gmail.com>
2023-11-22 16:33:00 +0000

    fix(revert package lock): revert package lock

--- src/components/organisms/Carousel/Carousel/Carousel.tsx
+++ src/components/organisms/Carousel/Carousel/Carousel.tsx
@@ -46,7 +46,6 @@ const CarouselComponent = ({ children, initialSlideIndex }: CarouselComponentPro
             <FlexContainer gutter={0}>
               <FlexRow cols={slidesCount} justify="space-around">
                 {Children.toArray(children).map(
-                  //@ts-expect-error TODO: refactor legacy cloneElement
                   (child, index) => isValidElement(child) && cloneElement(child, { index }),
                 )}
               </FlexRow>
